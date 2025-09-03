87161b5fedd8afa40dda3da603ea9729587f16ef
MafazaAnanda <rahmanmafaza0@gmail.com>
2025-09-03 22:35:11 +0700

    add item model and apply initial migration

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    CATEGORY_CHOICES = [
+    ('jersey', 'Jersey'),
+    ('shoes', 'Shoes'),
+    ('ball', 'Ball'),
+    ('accessory', 'Accessory'),
+    ('equipment', 'Equipment'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    description = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=25, choices=CATEGORY_CHOICES, default='update')
+    is_featured = models.BooleanField(default=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    stock = models.PositiveIntegerField(default=0)
+    brand = models.CharField(max_length=25, blank=True)
+
+def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406401306',
+        'name': 'Mafaza Ananda Rahman',
+        'class': 'PBP E'
+    }
+
+    return render(request, "main.html", context)
