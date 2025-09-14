f675ae4cac9415e98e3e24a04c87fc78f5859d68
MafazaAnanda <rahmanmafaza0@gmail.com>
2025-09-14 14:08:25 +0700

    add views and routes for XML and JSON

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, add_item, show_item
+from main.views import show_main, add_item, show_item, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('add-item/', add_item, name='add_item'),
     path('item/<str:id>/' , show_item, name="show_item"),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import Itemsform
 from main.models import Item
@@ -31,3 +33,29 @@ def show_item(request, id):
     }
 
     return render(request, "item_detail.html", context)
+
+def show_xml(request):
+    item_list = Item.objects.all()
+    xml_data = serializers.serialize("xml", item_list)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    item_list = Item.objects.all()
+    json_data = serializers.serialize("json", item_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_xml_by_id(request, item_id):
+    try:
+        item = Item.objects.filter(pk=item_id)
+        xml_data = serializers.serialize("xml", item)
+        return HttpResponse(xml_data, content_type="application/xml")
+    except Item.DoesNotExist:
+        return HttpResponse(status=404)
+
+def show_json_by_id(request, item_id):
+    try:
+        item = Item.objects.filter(pk=item_id)
+        json_data = serializers.serialize("json", item)
+        return HttpResponse(json_data, content_type="application/json")
+    except Item.DoesNotExist:
+        return HttpResponse(status=404)
