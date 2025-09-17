5d6f15a00a9cea5d576a6dd51a59506237ecaf94
MafazaAnanda <rahmanmafaza0@gmail.com>
2025-09-17 22:59:17 +0700

    add login and logout

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, add_item, show_item, show_xml, show_json, show_xml_by_id, show_json_by_id, register
+from main.views import show_main, add_item, show_item, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -12,4 +12,6 @@ urlpatterns = [
     path('xml/<str:item_id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:item_id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
