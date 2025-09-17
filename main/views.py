5e7f784aa4abac0d40ac3e5a016d42dc683043cf
MafazaAnanda <rahmanmafaza0@gmail.com>
2025-09-17 23:21:24 +0700

    fix typo

--- main/views.py
+++ main/views.py
@@ -78,33 +78,34 @@ def show_json_by_id(request, item_id):
     
 def register(request):
     form = UserCreationForm()
-
+    
     if request.method == "POST":
         form = UserCreationForm(request.POST)
+
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response     
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
         
     context = {'form' : form}
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('main:show_main')
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        
+    else:
+        form = AuthenticationForm(request)
 
-   else:
-      form = AuthenticationForm(request)
-
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    context = {'form' : form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
